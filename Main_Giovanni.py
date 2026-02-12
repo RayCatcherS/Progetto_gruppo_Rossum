a66e03fb002a2f1ba02347136f9c868b7de4d70b
Stefano Romanelli <53661102+RayCatcherS@users.noreply.github.com>
2026-02-12 17:21:44 +0100

    fix giovanni

--- Main_Giovanni.py
+++ Main_Giovanni.py
@@ -1,28 +1,30 @@
 from Main_Stefano import Elettrodomestico
 
 class Lavatrice(Elettrodomestico):
-    def __init__(self, marca: str, modello: str, anno: int, guasto: str, kg: int):
+    def __init__(self, marca: str, modello: str, anno: int, guasto: str, kg: int, giri: int):
         # Passiamo i dati comuni alla classe base Elettrodomestico
         super().__init__(marca, modello, anno, guasto)
         # kg è l'attributo unico e privato della lavatrice
         self.__kg = kg
+        self.__giri = giri
 
     # Getter per i kg
     def get_kg(self) -> int:
         return self.__kg
+    
+    def get_giri(self) -> int:
+        return self.__giri
 
     # Setter per i kg
     def set_kg(self, kg: int):
         if kg > 0:
             self.__kg = kg
 
-    # Override della descrizione
-    def descrizione(self) -> str:
-        # Usiamo i getter per tutto: quelli del padre (get_marca) e il tuo (get_kg)
-        return f"Lavatrice {self.get_marca()} {self.get_modello()} da {self.get_kg()}kg - Guasto: {self.get_guasto()}"
+    def set_giri(self, giri: int):
+        self.__giri = giri
 
     # Override Polimorfismo con super()
-    def stima_costa_base(self) -> float:
+    def stima_costa_base(self) -> int:
         # Chiamiamo il metodo del padre per avere i 100 euro iniziali
         costo_partenza = super().stima_costa_base() 
         

e171f31c970a6128bcc2fefd287d1ffa1fae0915
Stefano Romanelli <53661102+RayCatcherS@users.noreply.github.com>
2026-02-12 17:08:21 +0100

    modifica main stefano

--- Main_Stefano.py
+++ Main_Stefano.py
@@ -36,11 +36,32 @@ class Elettrodomestico():
             return True
 
     def descrizione(self) -> str:
-        pass
+        return f"marca: {self.__marca}, modello: {self.__modello}, anno: {self.__anno}, guasto: {self.__guasto}"
 
     def stima_costa_base(self) -> int:
-        pass
+        costo_base = 100
+        return costo_base
             
 
     def set_guasto(self, guasto: str):
         self.__guasto = guasto
+
+
+class Frigorifero(Elettrodomestico): 
+
+    soglia_litri = 100
+
+    def __init__(self, marca: str, modello: str, anno: int, guasto: str, litri: int, ha_freezer: bool):
+        super().__init__(marca, modello, anno, guasto)
+        self.__litri = litri 
+        self.__ha_freezer = ha_freezer 
+
+    def stima_costa_base(self) -> int:
+        costo = super().stima_costa_base()
+
+        if self.__ha_freezer:
+            costo = costo + 50
+
+        if self.soglia_litri < self.__litri:
+            costo = costo + 50
+        return costo

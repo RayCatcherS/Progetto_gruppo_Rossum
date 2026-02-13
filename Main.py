5e639a3d478e96c221b16aa8c10a1379fe4fb9fd
Stefano Romanelli <53661102+RayCatcherS@users.noreply.github.com>
2026-02-13 15:59:44 +0100

    main okay

--- Main.py
+++ Main.py
@@ -1,4 +1,34 @@
 from Implementazioni.Lavatrice import Lavatrice
 from Implementazioni.Forno import Forno
 from Implementazioni.Frigorifero import Frigorifero
+from Implementazioni.Officina import Officina
+from Implementazioni.TicketRiparazione import TicketRiparazione
+
+
+def statistiche_per_tipo( tickets: list[TicketRiparazione]):
+    lavatrici = 0
+    forni = 0
+    frigoriferi = 0
+
+    for ticket in ticket:
+        if(type(ticket).__name__ == "Lavatrice"):
+             lavatrici += 1
+        elif(type(ticket).__name__ == "Forno"):
+            forni += 1
+        elif(type(ticket).__name__ == "Frigorifero"):
+            frigoriferi += 1
+    print("Numero di lavatrici: ", lavatrici)
+    print("Numero di forni: ", forni)
+    print("Numero di frigoriferi: ", frigoriferi)
+
+
+def main():
+
+    officina = Officina()
+    pass
+
+
+
+
+main()
 
